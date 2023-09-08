a88fe59dd56aab52263b1b3ee4af19f3648e4d6d
Dany Carneiro <pc23wsc@eduvaud.ch>
2023-09-08 15:47:12 +0200

    08.09.2023 message début + galère recherche random

--- MasterMind/Program.cs
+++ MasterMind/Program.cs
@@ -8,8 +8,28 @@ namespace MasterMind
 {
     internal class Program
     {
+        /// <summary>
+        /// Jeu mastermind
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+            string goal;
+            int tries = 0;
+            string guess;
+
+            Console.WriteLine("Bienvenue sur Mastermind!");
+            Console.WriteLine("Couleurs possibles: GYWRBMC");
+            Console.WriteLine("Devine le code en 4 couleurs.");
+            Console.ReadLine();
+
+
+            //à compléter avec un boucle
+            Console.WriteLine("Essai " + tries + 1);
+            guess = Console.ReadLine();
+
+
+         
         }
     }
 }
