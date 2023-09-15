c28333db48da599dea0e9eda91938b23cad70851
Dany Carneiro <pc23wsc@eduvaud.ch>
2023-09-15 15:44:31 +0200

    version 3 15.09.2023 code aléatoire + boucle juste/faux

--- MasterMind/Program.cs
+++ MasterMind/Program.cs
@@ -14,22 +14,145 @@ namespace MasterMind
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            string goal;
-            int tries = 0;
+            string goal = "GYWR";
+            char firstLetter;
+            char secondLetter;
+            char thirdLetter;
+            char fourthLetter;
+            int tries;
             string guess;
-
+            int ok;
+            int badPosition;
+             //message de bienvenue
             Console.WriteLine("Bienvenue sur Mastermind!");
             Console.WriteLine("Couleurs possibles: GYWRBMC");
-            Console.WriteLine("Devine le code en 4 couleurs.");
-            Console.ReadLine();
+            Console.WriteLine("Devine le code en 4 couleurs. \n");
+
+            //génère la suite à deviner aléatoirement
+            Random random = new Random();
+            int x1 = random.Next(7);
+            int x2 = random.Next(7);
+            int x3 = random.Next(7);
+            int x4 = random.Next(7);
+            //traduit le chiffre obtenu aléatoirement en lettre afin d'avoir la première, deuxième, troisième et quatrième position du code à deviner
+            switch (x1)
+            {
+                case 0:
+                    firstLetter = 'C';
+                    break;
+                case 1:
+                    firstLetter = 'G';
+                    break;
+                case 2:
+                    firstLetter = 'Y';
+                    break;
+                case 3:
+                    firstLetter = 'W';
+                    break;
+                case 4:
+                    firstLetter = 'R';
+                    break;
+                case 5:
+                    firstLetter = 'B';
+                    break;
+                case 6:
+                    firstLetter = 'M';
+                    break;
+            }
+            switch (x2)
+            {
+                case 0:
+                    firstLetter = 'C';
+                    break;
+                case 1:
+                    firstLetter = 'G';
+                    break;
+                case 2:
+                    firstLetter = 'Y';
+                    break;
+                case 3:
+                    firstLetter = 'W';
+                    break;
+                case 4:
+                    firstLetter = 'R';
+                    break;
+                case 5:
+                    firstLetter = 'B';
+                    break;
+                case 6:
+                    firstLetter = 'M';
+                    break;
+
+            }
+            switch (x3)
+            {
+                case 0:
+                    firstLetter = 'C';
+                    break;
+                case 1:
+                    firstLetter = 'G';
+                    break;
+                case 2:
+                    firstLetter = 'Y';
+                    break;
+                case 3:
+                    firstLetter = 'W';
+                    break;
+                case 4:
+                    firstLetter = 'R';
+                    break;
+                case 5:
+                    firstLetter = 'B';
+                    break;
+                case 6:
+                    firstLetter = 'M';
+                    break;
+            }
+            switch (x4)
+            {
+                case 0:
+                    firstLetter = 'C';
+                    break;
+                case 1:
+                    firstLetter = 'G';
+                    break;
+                case 2:
+                    firstLetter = 'Y';
+                    break;
+                case 3:
+                    firstLetter = 'W';
+                    break;
+                case 4:
+                    firstLetter = 'R';
+                    break;
+                case 5:
+                    firstLetter = 'B';
+                    break;
+                case 6:
+                    firstLetter = 'M';
+                    break;
+
+            }
 
 
-            //à compléter avec un boucle
-            Console.WriteLine("Essai " + tries + 1);
-            guess = Console.ReadLine();
 
+            //le joueur rentre une suite et la console compare avec la suite à deviner et lui répond ensuite si c'est juste ou faux
+            for (tries = 1; tries < 11; ++tries)
+            {
+                Console.Write("Essai " + tries + ": ");
+                guess = Console.ReadLine();
 
-         
+                if (String.Equals(goal, guess))
+                {
+                    Console.WriteLine("Bravo, tu as deviné en " + tries + " essai(s) !");
+                    Console.ReadLine();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Dommage, c'est faux :( \n");
+                }
+            }
         }
     }
 }
